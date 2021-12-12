3aa539edf6237f555bc1353cd751af06725e30cd
SURAJ SAFI <94732511+suraj-safi1@users.noreply.github.com>
2021-12-12 12:19:55 +0530

    Count With Tuples

---
+++ Count_in_tuples.py
@@ -0,0 +1,8 @@
+#tuple methods
+t=(1,2,4,5,6,4,7,4,7,4)
+a=int(input("enter number to count what is it present in tuple \n"))
+print("Counts the appereance of particular data in tuple is",t.count(a),"times")
+b=int(input("enter number to find the index in tuple \n"))
+print("Index of Given Number is ",t.index(b))
+
+
